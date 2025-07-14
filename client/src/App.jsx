abe6d031170b3ce64310707a96cb92aa7d3175e4
Chris Foreman <cforeman01@qub.ac.uk>
2025-07-14 17:18:35 +0100

    notifications added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { io } from "socket.io-client";
 import { Routes, Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import MainNavbar from './components/common/MainNavbar';
@@ -5,15 +8,38 @@ import Dashboard from './pages/Dashboard';
 import Detections from './pages/Detections';
 import PageNotFound from './pages/404';
 
-const App = () => (
-  <Container fluid className="p-2">
+const App = () => {
+  const socketRef = useRef();
+
+    useEffect(() => {
+        socketRef.current = io("http://localhost:3002");
+
+        socketRef.current.on("newDetection", (detection) => {
+            toast.success(
+              `New detection: ${detection.species || detection.common_name} at ${detection.detection_timestamp} (${Math.round(detection.confidence * 100)}%)`,
+              { position: "bottom-right", autoClose: 6000 }
+            );
+        });
+
+        return () => {
+            socketRef.current.off("newDetection");
+            socketRef.current.disconnect();
+        };
+    }, []);
+
+
+  return (
+    <Container fluid className="p-2">
     <MainNavbar />
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/detections" element={<Detections />} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
+    <ToastContainer />
   </Container>
-);
+  )
+  
+};
 
 export default App;
